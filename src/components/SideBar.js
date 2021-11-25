a186abf0dac4ceda1dd8cbc2b2fcae5001181653
Agostina Pavese <agostinap85@gmail.com>
2021-11-25 19:32:43 -0300

    mi first commit

---
+++ src/components/SideBar.js
@@ -0,0 +1,60 @@
+import React from "react"
+
+function SideBar() {
+    return(
+    <div>
+       
+		<ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+		
+			<a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+				<div className="sidebar-brand-icon">
+					<img class="w-100" src="assets/images/logo-DH.png" alt="Digital House"/>
+				</div>
+			</a>
+
+		
+			<hr className="sidebar-divider my-0"/>
+
+		
+			<li className="nav-item active">
+				<a className="nav-link" href="/">
+					<i className="fas fa-fw fa-tachometer-alt"></i>
+					<span>Dashboard - DH movies</span></a>
+			</li>
+
+		
+			<hr className="sidebar-divider"/>
+
+			<div className="sidebar-heading">Actions</div>
+
+			
+			<li className="nav-item">
+				<a className="nav-link collapsed" href="/">
+					<i className="fas fa-fw fa-folder"></i>
+					<span>Pages</span>
+				</a>
+			</li>
+
+			<li className="nav-item">
+				<a className="nav-link" href="/">
+					<i className="fas fa-fw fa-chart-area"></i>
+					<span>Charts</span></a>
+			</li>
+
+		
+			<li class="nav-item">
+				<a class="nav-link" href="/">
+					<i class="fas fa-fw fa-table"></i>
+					<span>Tables</span></a>
+			</li>
+
+		
+			<hr class="sidebar-divider d-none d-md-block"/>
+		</ul>
+
+    </div>
+
+    );
+}
+ export default SideBar;
