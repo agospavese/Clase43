0df7d5d147c87365350020ad8c89e34ae9fdd589
Agostina Pavese <agostinap85@gmail.com>
2021-11-28 16:46:30 -0300

    modificando componentes con sus props

--- src/components/ContentRowMovies.js
+++ src/components/ContentRowMovies.js
@@ -1,63 +1,49 @@
 import React from "react"
 import '../assets/css/app.css'
 
-function ContentRowMovies() {
+function ContentRowMovies(props) {
+    const titulo = props.titulos
+    const cifra = props.cifras
+    const colorBorde = props.colorBordes
+    const icono = props.iconos
     return (
 
 <div className="row">
 
 
-    <div className="col-md-4 mb-4">
-        <div className="card border-left-primary shadow h-100 py-2">
+    {titulo.map((item, i)=><><div key={item + i} className="col-md-4 mb-4">
+        <div className={colorBorde}>
             <div className="card-body">
                 <div className="row no-gutters align-items-center">
                     <div className="col mr-2">
-                        <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in Data Base</div>
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
+                        <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">{item}</div>
+                        <div className="h5 mb-0 font-weight-bold text-gray-800">{cifra[i]}</div>
                     </div>
                     <div className="col-auto">
-                        <i className="fas fa-film fa-2x text-gray-300"></i>
+                        <i className={icono}></i>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    
 
+    </>
+    )} 
 
-    <div className="col-md-4 mb-4">
-        <div className="card border-left-success shadow h-100 py-2">
-            <div className="card-body">
-                <div className="row no-gutters align-items-center">
-                    <div className="col mr-2">
-                        <div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
-                    </div>
-                    <div className="col-auto">
-                        <i className="fas fa-award fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div className="col-md-4 mb-4">
-        <div className="card border-left-warning shadow h-100 py-2">
-            <div className="card-body">
-                <div className="row no-gutters align-items-center">
-                    <div className="col mr-2">
-                        <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Actors quantity
-                        </div>
-                        <div className="h5 mb-0 font-weight-bold text-gray-800">49</div>
-                    </div>
-                    <div className="col-auto">
-                        <i className="fas fa-user fa-2x text-gray-300"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+   
 </div>
+      
     )
 }
+  
+
+ContentRowMovies.defaultProps = {
+    titulo: "Ingrese un título",
+    cifra: "Ingrese una cifra",
+    colorBorde: "Ingrese un color de borde",
+    icono: "Ingrese un ícono"
+}
+
 
 export default ContentRowMovies;
--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -8,7 +8,7 @@ import GenresInDb from "./GenresInDb"
 function ContentRowTop() {
 	return (
 		<div>
-			<ContentRowMovies />
+			<ContentRowMovies  titulos={["Movies in Data Base", "Total awards", "Actors quantity"]} cifras={[21, 79, 49]} iconos={["fas fa-film fa-2x text-gray-300", "fas fa-award fa-2x text-gray-300", "fas fa-user fa-2x text-gray-300"]} colorBordes={["card border-left-primary shadow h-100 py-2","card border-left-success shadow h-100 py-2","card border-left-warning shadow h-100 py-2"]}/>
 			<LastMovieInDb />
 			<GenresInDb />
 		</div>
